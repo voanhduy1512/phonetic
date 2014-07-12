ba1810dd1f64027e3f699cdbff921b324d00c561
Hieu Nguyen <hieuk09@gmail.com>
2014-07-12 07:17:09 +0700

    [r] Refactor translator and word
    - Apply best practice
    - Improve performance

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,5 @@
 class Word < ActiveRecord::Base
   def self.new_word(word, word_phonetics)
-
     record = Word.new
     record.text = word
     arr = Array.new
@@ -16,21 +15,17 @@ class Word < ActiveRecord::Base
     if is_phonetic p
       return PHONETICS[p]
     end
+
     stress = p.last.match(/\d$/)
     if stress
       d = p[0..-2]
       if is_phonetic d
-        # if stress.to_s == '1'
-        #   data = "ˈ#{PHONETICS[d]}"
-        # elsif stress.to_s == '2'
-        #   data = "ˌ#{PHONETICS[d]}"
-        # else
-          data = "#{PHONETICS[d]}"
-        # end
+        data = "#{PHONETICS[d]}"
         return data
       end
     end
-    return ''
+
+    ''
   end
 
   def self.is_phonetic(text)
